476f13730e706288dc3bec707324008341fb72ab
lguerdan <lguerdan@yahoo.com>
2017-09-09 04:38:28 -0400

    add heroku mapping

--- app/app.py
+++ app/app.py
@@ -5,28 +5,30 @@ import requests, base64
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/new", methods=['POST', 'GET'])
 def process_image():
+
+   print request.json
+
 
    with open("images/sample1.jpg", "rb") as image_file:
        encoded_string = base64.b64encode(image_file.read())
 
-   request = {}
+   img_request = {}
    rtype = {}
-   request["image"] = {"content" : encoded_string}
-   request["features"] = []
-   request["features"].append({"type": "TEXT_DETECTION"})
+   img_request["image"] = {"content" : encoded_string}
+   img_request["features"] = []
+   img_request["features"].append({"type": "TEXT_DETECTION"})
 
    payload = {}
    payload["requests"] = []
-   payload["requests"].append(request)
+   payload["requests"].append(img_request)
 
    r = requests.post("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyA-ChOP_rd3Ny2_n8vgQfpY-sViFx3weU0", data=json.dumps(payload))
    respjson = json.loads(r.text)
    message = respjson['responses'][0]['fullTextAnnotation']['text']
 
    return message
-
 
 
 if __name__ == '__main__':
