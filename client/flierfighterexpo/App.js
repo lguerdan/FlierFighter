e48ea638a34593ce6373e5240d84e3a4e1f510fd
Michael Gardner <gardnm4@rpi.edu>
2017-09-10 03:43:10 -0400

    receive data from server, place it into a form, format it a bit, and a submmit button

--- client/flierfighterexpo/App.js
+++ client/flierfighterexpo/App.js
@@ -6,22 +6,50 @@ import {
   View,
   TouchableOpacity,
   Image,
-  CameraRoll
+  CameraRoll,
+  TextInput,
 } from 'react-native';
+import {Button, COLOR, ThemeProvider } from 'react-native-material-ui';
 
 
 const GOOGLE_VISION_API = 'AIzaSyAchuzZ-iR_Mswd5WUYFOgck8MRP6AQnM0';
 const GOOGLE_VISION_URL = 'https://vision.googleapis.com/v1/images:annotate?key=';
+const FLIERFIGHTER_URL ='https://fliers.herokuapp.com/getImage';
+
+const uiTheme = {
+    palette: {
+        primaryColor: COLOR.green500,
+    },
+    button: {
+        container: {
+            height: 50,
+        },
+    },
+};
+
+
+export default class Main extends Component {
+  render() {
+        return (
+            <ThemeProvider uiTheme={uiTheme}>
+                <App />
+            </ThemeProvider>
+        );
+    }
+}
 
 
 
-
-
-
-export default class App extends Component {
+class App extends Component {
   state = {
     imageUri : null,
     text : null,
+    ORC : false,
+    eventDetail : false,
+    datetime_from:null,
+    datetime_to:null,
+    location:null,
+    title:null,
   }
 
   render(){
@@ -44,7 +72,11 @@ export default class App extends Component {
       )
     }
 
-    // if (this.state.imageUri){
+    if (this.state.OCR && !this.state.eventDetail){
+      this.sendText(this.state.text);
+    }
+
+    if (!this.state.eventDetail){
       return(
         <View style = {styles.container}>
           {imageView}
@@ -61,9 +93,64 @@ export default class App extends Component {
           </TouchableOpacity>
         </View>
       )
+    }
+    else {
+      return (
+        <View style = {styles.container}>
+          <Text>Please Correct Gathered Info</Text>
+          <TextInput
+            style={{height: 80, borderColor: 'gray', borderWidth: 1, width: '80%',margin: 10}}
+            onChangeText={(title) => this.setState({title})}
+            value={this.state.title}
+            // multiline = {true}
+            returnKeyType = 'done'
+          />
+          <TextInput
+            style={{height: 80, borderColor: 'gray', borderWidth: 1, width: '80%',margin: 10}}
+            onChangeText={(location) => this.setState({location})}
+            value={this.state.location}
+            // multiline = {true}
+            returnKeyType = 'done'
+          />
+          <TextInput
+            style={{height: 80, borderColor: 'gray', borderWidth: 1, width: '80%',margin: 10}}
+            onChangeText={(datetime_from) => this.setState({datetime_from})}
+            value={this.state.datetime_from}
+            // multiline = {true}
+            returnKeyType = 'done'
+          />
+          <TextInput
+            style={{height: 80, borderColor: 'gray', borderWidth: 1, width: '80%',margin: 10}}
+            onChangeText={(datetime_to) => this.setState({datetime_to})}
+            value={this.state.datetime_to}
+            // multiline = {true}
+            returnKeyType = 'done'
+          />
+          <Button
+            onPress={() => this.submitCorrections()}
+            text="Submit Corrections"
+            primary
+            raised
+          />
+        </View>
+      )
+    }
   }
 
+  submitCorrections = () => {
+    console.log('go!')
+  }
 
+  timestampToHRD = (ts) => {
+    if (ts.length > 0){
+      let re = /-/;
+      ts = ts.replace(/-/g,'/');
+      ts = ts.replace(/T/g,'\t');
+      ts = ts.slice(0,-10);
+    }
+
+    return ts
+  }
 
   openImage = async () => {
     const {
@@ -74,7 +161,8 @@ export default class App extends Component {
     if(!cancelled){
       this.setState({
         imageUri : uri,
-        text : '(loading...)'
+        text : '(loading...)',
+        OCR : false,
       });
     }
 
@@ -105,11 +193,14 @@ export default class App extends Component {
     });
 
     const parsed = await response.json();
+    const text = parsed.responses[0]['textAnnotations'][0]['description'].toString();
     this.setState({
-      text: parsed.responses[0]['textAnnotations'][0]['description']
+      text, OCR : true
     })
+    // this.sendText(text)
 
   }
+
 
 
   captureImage = async () => {
@@ -123,7 +214,8 @@ export default class App extends Component {
     if(!cancelled){
       this.setState({
         imageUri : uri,
-        text : '(loading...)'
+        text : '(loading...)',
+        OCR : false
       });
     }
 
@@ -153,9 +245,39 @@ export default class App extends Component {
     });
 
     const parsed = await response.json();
+    const text = parsed.responses[0]['textAnnotations'][0]['description'].toString();
     this.setState({
-      text: parsed.responses[0]['textAnnotations'][0]['description']
+      text, OCR:true
     })
+
+  }
+
+
+  sendText = async (text) => {
+    console.log('hi')
+    const response = await fetch(`${FLIERFIGHTER_URL}`, {
+      method : 'POST',
+      headers : {
+        'Accept' : 'application/json',
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify({'imageData': text})
+    });
+    const eventDetail = await response.json();
+    const {datetime_from,datetime_to,location,title} = {...eventDetail}
+    console.log(datetime_from,datetime_to)
+    this.setState({eventDetail:true,datetime_from : this.timestampToHRD(datetime_from),datetime_to : this.timestampToHRD(datetime_to),location,title, ORC:false});
+    // try {
+    //   let eventDetail = await response.json();
+    //   // let eventDetail = parsedText.json();
+    //   // eventDetail.title = eventDetail.title.replace('\\\"')
+    //   this.setState({eventDetail})
+    //   // console.log("herokuapp",parsed)
+    // } catch(e){
+    //   let parsedText = await response.text();
+    //   console.log("ERROR:",parsedText,e)
+    // }
+
 
   }
 }
